5e2f2eff3fa2796fd71b0af3d8a588b2199ed2fe
v-ca <cloudsarefunky@gmail.com>
2019-11-17 17:12:53 -0700

    file(cryptomath.py and environment.yml): Initial commit

---
+++ cryptomath.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def gcd(a, b):
+    divisor = np.gcd(a, b)
+    return divisor
+
+
+def findModInverse(a, m):
+    # Returns the modular inverse of a % m, which is
+    # the number x such that a*x % m = 1
+
+    if gcd(a, m) != 1:
+        return None # no mod inverse if a & m aren't relatively prime
+
+    # Calculate using the Extended Euclidean Algorithm:
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    while v3 != 0:
+        q = u3 // v3 # // is the integer division operator
+        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
+    return u1 % m
+
+def egcd(a, b):
+    x, y, u, v = 0, 1, 1, 0
+    while a != 0:
+        q, r = b // a, b % a
+        m, n = x - u * q, y - v * q
+        b, a, x, y, u, v = a, r, u, v, m, n
+    gcd = b
+    return gcd, x, y
+
+
+def modinv(a, m):
+    gcd, x, y = egcd(a, m)
+    if gcd != 1:
+        return None  # modular inverse does not exist
+    else:
+        return x % m
